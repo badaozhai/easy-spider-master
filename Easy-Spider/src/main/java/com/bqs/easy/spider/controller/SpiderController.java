925256101bb3f95c672afb155aa361b78a419251
51ttw <329966505@qq.com>
2015-08-30 22:38:21 +0800

    更改包名

--- Easy-Spider/src/main/java/com/bqs/easy/spider/controller/SpiderController.java
+++ Easy-Spider/src/main/java/com/bqs/easy/spider/controller/SpiderController.java
@@ -1,12 +1,12 @@
-package com.tan.controller;
+package com.bqs.easy.spider.controller;
 
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/book.do")
-public class BookController {
+@RequestMapping("/spider.do")
+public class SpiderController {
 
 	@RequestMapping(params = "method=update")
 	public String update() {
