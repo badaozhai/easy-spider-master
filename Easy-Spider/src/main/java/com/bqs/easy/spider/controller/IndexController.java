e116aba79ae4e3c52f491ab6444be907db7ee8c5
51ttw <329966505@qq.com>
2015-08-31 22:32:11 +0800

    登陆拦截，未成功

--- Easy-Spider/src/main/java/com/bqs/easy/spider/controller/IndexController.java
+++ Easy-Spider/src/main/java/com/bqs/easy/spider/controller/IndexController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.bqs.easy.spider.entity.Task;
 import com.bqs.easy.spider.entity.User;
 
 @Controller
@@ -13,7 +12,7 @@ public class IndexController {
 
 	// @RequestMapping(params = "/index.html")
 	@RequestMapping({ "/index.html" })
-	public String index(@ModelAttribute Task t, @RequestHeader("User-Agent") String userAgent, @RequestHeader("cookie") String cookie) {
+	public String index(@RequestHeader("User-Agent") String userAgent, @RequestHeader("cookie") String cookie) {
 		if (userAgent.contains("Mobile")) {
 			return "mindex";
 		} else {
@@ -21,18 +20,9 @@ public class IndexController {
 		}
 	}
 
-	@RequestMapping({ "/login.html" })
-	public String login(@ModelAttribute Task t, @RequestHeader("User-Agent") String userAgent) {
-		if (userAgent.contains("Mobile")) {
-			return "mlogin";
-		} else {
-			return "login";
-		}
-	}
-	
 	@RequestMapping({ "/login" })
-	public String update(@ModelAttribute User u) {
-		if (u != null&&!"".equals(u.getUsername())) {
+	public String update(@ModelAttribute User u, @RequestHeader("cookie") String cookie) {
+		if (u != null && !"".equals(u.getUsername())) {
 			System.out.println(u);
 		} else {
 			System.out.println(null + "");
