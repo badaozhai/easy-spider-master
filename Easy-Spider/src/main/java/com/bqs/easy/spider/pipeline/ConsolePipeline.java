edf4b0c5ffd0558847b6af7eec0b19aed0df13da
51ttw <329966505@qq.com>
2015-09-06 10:16:29 +0800

    网页内容去标签，输出到磁盘

--- Easy-Spider/src/main/java/com/bqs/easy/spider/pipeline/ConsolePipeline.java
+++ Easy-Spider/src/main/java/com/bqs/easy/spider/pipeline/ConsolePipeline.java
@@ -1,10 +1,14 @@
 package com.bqs.easy.spider.pipeline;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
+import org.jsoup.Jsoup;
 
 import com.bqs.easy.httpclient.entity.Page;
 import com.bqs.easy.spider.imp.IPipeline;
@@ -16,13 +20,16 @@ public class ConsolePipeline implements IPipeline {
 
 	@Override
 	public Object process(Page p) {
-		return p.getHtml().length();
+		return Jsoup.parse(p.getHtml()).text().replaceAll("[\n\t]", "");
 	}
 
 	@Override
 	public void save() {
-		for (Object object : list) {
-			log.info(object);
+		log.info("save");
+		try {
+			FileUtils.writeLines(new File("data"+File.separator+System.currentTimeMillis()+".txt"), list);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 
